c256c654fb297a3ef6ca3705af72114243eb1a62
songihyeon <tyumr70@gmail.com>
2025-02-18 17:22:36 +0900

    task completed

--- script.js
+++ script.js
@@ -6,12 +6,29 @@ const operator = document.querySelector('.calculator__operator'); // calculator_
 const secondOperend = document.querySelector('.calculator__operend--right'); // calculator__operend--right 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
 const calculatedResult = document.querySelector('.calculator__result'); // calculator__result 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
 
-function calculate(n1, operator, n2) {
-  let result = 0;
-  // TODO : n1과 n2를 operator에 따라 계산하는 함수를 만드세요.
-  // ex) 입력값이 n1 : '1', operator : '+', n2 : '2' 인 경우, 3이 리턴됩니다.
-  return String(result);
-}
+const reset_screen = document.querySelector('.clear');
+
+const operend = document.querySelector('.calculator__display--for-advanced');
+
+// 화면 초기화 이벤트
+// reset_screen.addEventListener('click', function () {
+//   firstOperend.textContent = '0';
+//   operator.textContent = '+';
+//   secondOperend.textContent = '0';
+//   calculatedResult.textContent = '0';
+// })
+
+// 연산 결과문 조건문
+// if (target.matches('button')) {
+//   if (action === 'calculate') {
+//     const result = calculate(
+//       firstOperend.textContent,
+//       operator.textContent,
+//       secondOperend.textContent
+//     );
+//     calculatedResult.textContent = result;
+//   }
+// }
 
 buttons.addEventListener('click', function (event) {
   // 버튼을 눌렀을 때 작동하는 함수입니다.
@@ -27,48 +44,60 @@ buttons.addEventListener('click', function (event) {
     if (action === 'number') {
       // 그리고 버튼의 클레스가 number이면
       // 아래 코드가 작동됩니다.
-      console.log('숫자 ' + buttonContent + ' 버튼');
-    }
-
-    if (action === 'operator') {
-      console.log('연산자 ' + buttonContent + ' 버튼');
-    }
-
-    if (action === 'decimal') {
-      // console.log('소수점 버튼');
-    }
-
-    if (action === 'clear') {
-      console.log('초기화 버튼');
-    }
-
-    if (action === 'calculate') {
-      console.log('계산 버튼');
+      if (firstOperend.textContent === '0') {
+        firstOperend.textContent = buttonContent;
+      } else {
+        secondOperend.textContent = buttonContent;
+      }
     }
   }
-});
 
-
-// ! Advanced Challenge test와 Nightmare test를 위해서는 아래 주석을 해제하세요.
-
-const display = document.querySelector('.calculator__display--for-advanced'); // calculator__display 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
-let firstNum, operatorForAdvanced, previousKey, previousNum;
-
-buttons.addEventListener('click', function (event) {
-  // 버튼을 눌렀을 때 작동하는 함수입니다.
-
-  const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
-  const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
-  const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
-  // ! 위 코드는 수정하지 마세요.
-
-  // ! 여기서부터 Advanced Challenge & Nightmare 과제룰 풀어주세요.
-  if (target.matches('button')) {
-    if (action === 'number') {}
-    if (action === 'operator') {}
-    if (action === 'decimal') {}
-    if (action === 'clear') {}
-    if (action === 'calculate') {}
+  if (action === 'operator') {
+    operator.textContent = buttonContent;
   }
 
-});
+  if (action === 'decimal') {
+    operator.textContent = buttonContent;
+  }
+
+  if (action === 'clear') {
+    firstOperend.textContent = '0';
+    operator.textContent = '+';
+    secondOperend.textContent = '0';
+    calculatedResult.textContent = '0';
+  }
+
+  if (action === 'calculate') {
+    const result = calculate(
+      firstOperend.textContent,
+      operator.textContent,
+      secondOperend.textContent
+    );
+    calculatedResult.textContent = result;
+  }
+}
+);
+
+function calculate(n1, operator, n2) {
+  let result = 0;
+  // TODO : n1과 n2를 operator에 따라 계산하는 함수를 만드세요.
+  // ex) 입력값이 n1 : '1', operator : '+', n2 : '2' 인 경우, 3이 리턴됩니다.
+
+  n1 = Number(n1);
+  n2 = Number(n2);
+
+  if (operator === '+') {
+    result = n1 + n2;
+  }
+  else if (operator === '-') {
+    result = n1 - n2;
+  }
+  else if (operator === '*') {
+    result = n1 * n2;
+  }
+  else if (operator === '/') {
+    result = n1 / n2;
+  }
+
+  return String(result);
+}
