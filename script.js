623d2681deb59f3a20108cf7bc9fc25d52d4c5f2
songihyeon <tyumr70@gmail.com>
2025-02-24 19:03:25 +0900

    advanced version modified

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ const calculatedResult = document.querySelector('.calculator__result'); // calcu
 
 const reset_screen = document.querySelector('.clear');
 
-const operend = document.querySelector('.calculator__display--for-advanced');
-
 // 화면 초기화 이벤트
 // reset_screen.addEventListener('click', function () {
 //   firstOperend.textContent = '0';
@@ -30,53 +28,53 @@ const operend = document.querySelector('.calculator__display--for-advanced');
 //   }
 // }
 
-buttons.addEventListener('click', function (event) {
-  // 버튼을 눌렀을 때 작동하는 함수입니다.
+// buttons.addEventListener('click', function (event) {
+// 버튼을 눌렀을 때 작동하는 함수입니다.
 
-  const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
-  const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
-  const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
-  // ! 위 코드(Line 19 - 21)는 수정하지 마세요.
+// const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
+// const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
+// const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
+// ! 위 코드(Line 19 - 21)는 수정하지 마세요.
 
-  if (target.matches('button')) {
-    // TODO : 계산기가 작동할 수 있도록 아래 코드를 수정하세요. 작성되어 있는 조건문과 console.log를 활용하시면 쉽게 문제를 풀 수 있습니다.
-    // 클릭된 HTML 엘리먼트가 button이면
-    if (action === 'number') {
-      // 그리고 버튼의 클레스가 number이면
-      // 아래 코드가 작동됩니다.
-      if (firstOperend.textContent === '0') {
-        firstOperend.textContent = buttonContent;
-      } else {
-        secondOperend.textContent = buttonContent;
-      }
-    }
-  }
+//   if (target.matches('button')) {
+//     // TODO : 계산기가 작동할 수 있도록 아래 코드를 수정하세요. 작성되어 있는 조건문과 console.log를 활용하시면 쉽게 문제를 풀 수 있습니다.
+//     // 클릭된 HTML 엘리먼트가 button이면
+//     if (action === 'number') {
+//       // 그리고 버튼의 클레스가 number이면
+//       // 아래 코드가 작동됩니다.
+//       if (firstOperend.textContent === '0') {
+//         firstOperend.textContent = buttonContent;
+//       } else {
+//         secondOperend.textContent = buttonContent;
+//       }
+//     }
+//   }
 
-  if (action === 'operator') {
-    operator.textContent = buttonContent;
-  }
+//   if (action === 'operator') {
+//     operator.textContent = buttonContent;
+//   }
 
-  if (action === 'decimal') {
-    operator.textContent = buttonContent;
-  }
+//   if (action === 'decimal') {
+//     operator.textContent = buttonContent;
+//   }
 
-  if (action === 'clear') {
-    firstOperend.textContent = '0';
-    operator.textContent = '+';
-    secondOperend.textContent = '0';
-    calculatedResult.textContent = '0';
-  }
+//   if (action === 'clear') {
+//     firstOperend.textContent = '0';
+//     operator.textContent = '+';
+//     secondOperend.textContent = '0';
+//     calculatedResult.textContent = '0';
+//   }
 
-  if (action === 'calculate') {
-    const result = calculate(
-      firstOperend.textContent,
-      operator.textContent,
-      secondOperend.textContent
-    );
-    calculatedResult.textContent = result;
-  }
-}
-);
+//   if (action === 'calculate') {
+//     const result = calculate(
+//       firstOperend.textContent,
+//       operator.textContent,
+//       secondOperend.textContent
+//     );
+//     calculatedResult.textContent = result;
+//   }
+// }
+// );
 
 function calculate(n1, operator, n2) {
   let result = 0;
@@ -101,3 +99,60 @@ function calculate(n1, operator, n2) {
 
   return String(result);
 }
+
+// ! Advanced Challenge test와 Nightmare test를 위해서는 아래 주석을 해제하세요.
+
+const display = document.querySelector('.calculator__display--for-advanced'); // calculator__display 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
+let firstNum, operatorForAdvanced, previousKey, previousNum;
+
+buttons.addEventListener('click', function (event) {
+  // 버튼을 눌렀을 때 작동하는 함수입니다.
+
+  const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
+  const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
+  const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
+  // ! 위 코드는 수정하지 마세요.
+
+  // ! 여기서부터 Advanced Challenge & Nightmare 과제룰 풀어주세요.
+
+  if (target.matches('button')) {
+
+    if (action === 'number') {
+      if (previousKey === 'operator') {
+        display.textContent = buttonContent;
+        previousKey = 'number';
+      } else {
+        if (display.textContent === '0') {
+          display.textContent = buttonContent;
+        } else {
+          display.textContent.textContent + buttonContent;
+        }
+      }
+    }
+
+    if (action === 'operator') {
+      operatorForAdvanced = buttonContent;
+      previousKey = 'operator';
+      firstNum = display.textContent;
+    }
+
+    if (action === 'decimal') {
+    }
+
+    if (action === 'clear') {
+      display.textContent = '0';
+      firstNum = 0;
+      operatorForAdvanced = '';
+      previousKey = '';
+      previousNum = '';
+    }
+
+    if (action === 'calculate') {
+      display.textContent = calculate(
+        firstNum,
+        operatorForAdvanced,
+        display.textContent
+      );
+    }
+  }
+});
